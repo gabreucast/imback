ac2b408ae97aa87f769b3ab3cd21312ae7b8420e
Gabriel <gabreucast@gmail.com>
2025-11-23 17:01:47 +0100

    added: 1. createGuidelineFromTop. 2. createEndBarrier. 3. createVerticalChain -> ChainStyle: Packed, Spread, SpreadInside

--- app/src/main/java/com/example/imback/components/layout/MyReto2.kt
+++ app/src/main/java/com/example/imback/components/layout/MyReto2.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Cyan
 import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun MyReto2(modifier: Modifier = Modifier) {
     ConstraintLayout(Modifier
@@ -27,7 +30,7 @@ fun MyReto2(modifier: Modifier = Modifier) {
                     bottom.linkTo(parent.bottom)
                     top.linkTo(parent.top)
                     end.linkTo(parent.end)
-                    start.linkTo(parent.start)
+                    start.linkTo(parent.start, )
                 })
         Box(
             modifier
@@ -106,7 +109,7 @@ fun MyReto2(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun ConstrainsExampleGuide(modifier: Modifier = Modifier){
     ConstraintLayout(modifier.fillMaxSize()) {
@@ -121,12 +124,60 @@ fun ConstrainsExampleGuide(modifier: Modifier = Modifier){
 
 //@Preview(showBackground = true)
 @Composable
-fun ConstraintBarrier(modifier: Modifier){
+fun ConstraintBarrier(modifier: Modifier = Modifier){
     ConstraintLayout(modifier.fillMaxSize()) {
-        val (boxRed, boxYellow) = createRefs()
+        val (boxRed, boxYellow, boxCyan) = createRefs()
+        val barrier = createEndBarrier(boxRed, boxYellow)
 
         Box(Modifier.size(65.dp).background(Red).constrainAs(boxRed){
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            })
 
+        Box(modifier.size(200.dp).background(Yellow).constrainAs(boxYellow){
+            start.linkTo(boxRed.start, margin = 40.dp)
+            top.linkTo((boxRed.bottom), 35.dp)
+        })
+
+        Box(modifier.size(70.dp).background(Cyan).constrainAs(boxCyan){
+            start.linkTo(barrier)
         })
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ConstrainChain(modifier: Modifier = Modifier){
+    ConstraintLayout(modifier.fillMaxSize()) {
+        val (boxRed, boxYellow, boxCyan) = createRefs()
+
+        Box(Modifier.size(100.dp).background(Red).constrainAs(boxRed) {
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            top.linkTo(parent.top)
+            bottom.linkTo(boxYellow.top)
+
+        })
+
+        Box(modifier.size(100.dp).background(Yellow).constrainAs(boxYellow) {
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            top.linkTo(boxRed.bottom)
+            bottom.linkTo(boxCyan.top)
+        })
+
+        Box(modifier.size(100.dp).background(Cyan).constrainAs(boxCyan) {
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            top.linkTo(boxYellow.bottom)
+            bottom.linkTo(parent.bottom)
+        })
+
+//        createVerticalChain(boxRed, boxYellow, boxCyan, chainStyle = ChainStyle.Packed)
+//        createVerticalChain(boxRed, boxYellow, boxCyan, chainStyle = ChainStyle.Spread)
+        createVerticalChain(boxRed, boxYellow, boxCyan, chainStyle = ChainStyle.SpreadInside)
+
+
+    }
+
 }
